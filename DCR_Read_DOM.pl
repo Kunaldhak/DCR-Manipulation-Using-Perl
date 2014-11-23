cb4730ce4dad36ebb6df3f7d389a2d0762c0afce
Enigma <kunaldhak1@gmail.com>
2014-11-24 03:09:56 +0530

    24/11

--- DCR_Read_DOM.pl
+++ DCR_Read_DOM.pl
@@ -6,36 +6,50 @@ use File::Copy;
 use File::Find;
 use Data::Dumper;
 use Encode;
-use MIME::EncWords qw ( /decode_mimewords/);
+use File::Basename;
+#######################################
+my $output_CSV="D:\\chinese.csv";     
+my @languages=('en_US','it_IT','zh_CN_cor');      ## Global Variables...
+my @content=('display_name'); 
+my $dcr_path = "D:\\Test_DCR\\zh_CN_cor"; 
+my @files_dcr;       
+#######################################
 
-my $output_CSV="D:\\chinese.csv";
+finddepth(\&load_dcr, $dcr_path);
 
-
-# my $locale ="en_US";	
-my $dcr = "D:\\zh_CN_cor";
-my @files_dcr;
-
-finddepth(\&wanted_dcr, $dcr);
-
-sub wanted_dcr 
-{	-f && push(@files_dcr, $File::Find::name);}
+sub load_dcr 
+{	
+	-f && push(@files_dcr, $File::Find::name);
+	
+}
 
 open (FILETAR,'>>', $output_CSV) or die "Could not open File : $!\n";
 
 foreach my $targetFile (@files_dcr)
-
 {
-	print $targetFile;
+	print "\n".$targetFile;
 	chomp($targetFile);
+	my $display_name=&get_value($targetFile);
+	#my $display_name1 = decode('HZ', $display_name);
+	#binmode STDOUT, ': ISO-8859-1';
+	my $file_name=basename($targetFile);
+	print FILETAR $file_name.",".$display_name."\n"
+	
+}
+
+sub get_value{
+	my ($targetFile)=@_;
 	my $p = XML::Parser->new( NoLWP => 1);
 	my $xp = XML::XPath->new(parser => $p, filename => $targetFile);
-	
 	my $display_name;
 	foreach my $xmlrepoNode  ($xp->find('/record/item[@name="display_name"]/value/text()')->get_nodelist)
 	{$display_name = $xmlrepoNode->getValue;}
-	my $test=decode_mimewords($display_name);
-	my $display_name1 = decode('HZ', $display_name);
-	#binmode STDOUT, ': ISO-8859-1';
-	print FILETAR $targetFile.",".$test."\n"
-	
+	return $display_name;
 }
+
+
+
+
+
+
+
