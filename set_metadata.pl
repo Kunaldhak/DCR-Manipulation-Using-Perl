3f9c6cdb59ef3fbcf413282f90ff4c341deb7c12
KUNALD1 <kunaldhak1@gmail.com>
2015-01-21 22:57:38 +0530

    21/1

--- set_metadata.pl
+++ set_metadata.pl
@@ -15,9 +15,24 @@ my $fname = $_;
 	my @pathArray3 = split(/\//,$pathArray2[0]);
 	my $data_type = $pathArray3[1]."/".$pathArray3[2];
 
-my $cmd="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Templating/DCR/Type=$data_type $fname";
-#qx($cmd);
-print "$cmd\n"; 
+my $cmd="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/global_content_type='local'  $fname";
+my $cmd1="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/global_content_type/core='y'  $fname";
+my $cmd2="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/need_to_translate='yes'   $fname";
+my $cmd3="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/need_to_translate/yes='y'  $fname";
+my $cmd4="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/target_languages='en-gb, it-it, pt-br, es-es, fr-fr'   $fname";
+my $cmd5="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/target_languages/es-es='y'  $fname";
+my $cmd6="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/target_languages/fr-fr='y'  $fname";
+my $cmd7="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/target_languages/it-it='y'  $fname";
+my $cmd8="f:\\iw-home\\TeamSite\\bin\\iwextattr.exe -s TeamSite/Metadata/target_languages/pt-br='y'  $fname";
+system($cmd);
+system($cmd2);
+system($cmd3);
+system($cmd4);
+system($cmd5);
+system($cmd6);
+system($cmd7);
+system($cmd8);
+#print "$cmd\n"; 
 } @files;
 
 sub loadFiles()
